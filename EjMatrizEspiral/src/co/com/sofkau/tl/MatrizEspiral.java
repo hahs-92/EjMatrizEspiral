fb87a90d9d88b4483de35bbb629ddfb2335ca40d
Alex <alexandre_2420@outlook.com>
2022-02-03 16:27:19 -0500

    DONE 4 functions to iterate upon matriz

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,13 +1,71 @@
 package co.com.sofkau.tl;
 
-public class MatrizEspiral {
-    // TODO: 2/02/2022 método para recorrer fila izq-der
-    // TODO: 2/02/2022 método para recorrer fila der-izq
-    // TODO: 2/02/2022 método para recorrer columna desc
-    // TODO: 2/02/2022 método para recorrer columna asc
-    // TODO: 2/02/2022 método para solicitar tamaño de matriz a llenar
-    // TODO: 2/02/2022 método para imprimir la matriz
-    // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
+
+public class MatrizEspiral {
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
+    public static int initialValue = 1;
+    public static int start = 0;
+    public static int numRow = 0;
+
+    public static void main(String[] args) {
+        int sizeMatriz = 3;
+        int[][] matriz = new int[sizeMatriz][sizeMatriz];
+        numRow = sizeMatriz;
+
+        matriz = fromLeftToRight(matriz,numRow, start);
+        matriz = fromToptoBottom(matriz, numRow, start);
+        matriz = fromRightToLeft(matriz, numRow, start);
+        matriz = fromBottomtoTop(matriz, numRow, start);
+
+        printMatriz(matriz);
+    }
+
+    // TODO: 2/02/2022 método para solicitar tamaño de matriz a llenar
+    // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
+   
+
+
+    // método para recorrer fila izq-der
+    public  static int[][] fromLeftToRight(int[][] matriz, int numRow, int start) {
+        for(int i = start; i <= numRow - 1; i++) {
+            matriz[start][i] = initialValue++;
+        }
+        return  matriz;
+    }
+
+    // método para recorrer columna desc
+    public  static int[][] fromToptoBottom(int[][] matriz, int numRow, int start) {
+        for(int i = start + 1; i <= numRow - 1 ; i++) {
+            matriz[i][numRow - 1] = initialValue++;
+        }
+        return  matriz;
+    }
+
+    // método para recorrer fila der-izq
+    public  static int[][] fromRightToLeft(int[][] matriz, int numRow, int start) {
+        for(int i = numRow - 2; i >= start ; i--) {
+            matriz[numRow - 1][i] = initialValue++;
+        }
+        return  matriz;
+    }
+
+    // método para recorrer columna asc
+    public  static int[][] fromBottomtoTop(int[][] matriz, int numRow, int start) {
+        for(int i = numRow - 2 ; i >= start + 1 ; i--) {
+            matriz[i][start] = initialValue++;
+        }
+        return  matriz;
+    }
+
+    // método para imprimir la matriz
+    public static void printMatriz(int[][] matriz) {
+        for(int i = 0; i < matriz.length; i++) {
+            for(int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "|");
+            }
+            System.out.println();
+        }
+    }
+
 }
